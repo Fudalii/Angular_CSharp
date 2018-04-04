d0891b234d9794685374aacdf14a175f6f84fc16
Kamil Fudali <fudalikamil@hotmail.com>
2018-04-04 16:40:05 +0200

    LikeButton and service SEND lIKE

--- DatingApp.SPA/src/app/members/member-card/member-card.component.ts
+++ DatingApp.SPA/src/app/members/member-card/member-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../../_models/User';
+import { AuthService } from '../../_services/auth.service';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
 
 @Component({
   selector: 'app-member-card',
@@ -7,12 +10,24 @@ import { User } from '../../_models/User';
   styleUrls: ['./member-card.component.less']
 })
 export class MemberCardComponent implements OnInit {
-
   @Input() user: User;
 
-  constructor() { }
+  constructor(
+    private _auth: AuthService,
+    private _user: UserService,
+    private _alert: AlertifyService
 
-  ngOnInit() {
+  ) {}
+
+  ngOnInit() {}
+
+
+  sendLike(id: number) {
+    this._user.sendLike(this._auth.decodeToken.nameid, id).subscribe(
+      data => { this._alert.success('You have liked: ' + this.user.knownAs); },
+      error => { this._alert.error(error); }
+    );
   }
+
 
 }
